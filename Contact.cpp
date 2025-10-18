22bbac343a668176087b0a87c6748e60d986e34d
htngo11 <hngo27@saddleback.edu>
2025-10-17 18:40:46 -0700

    Add files via upload

--- Contact.cpp
+++ Contact.cpp
@@ -1,192 +1,79 @@
+// Group Project: Address Book
+// Name: Hien, Cade, Noah, Austin, Isaiah, Nicholas
+// Date: 10/14/2025
+// Class: MW 6-6:50PM
+
+// Contact.cpp
 #include "Contact.h"
 
-// =============================================================================
-// MARK: Constructors
-// =============================================================================
-
-Contact::Contact() {
-    name = "";
-    email = "";
-    phoneNumber = "";
-    city = "";
-    type = PERSON;
-    groups = {};
-    tags = {};
-}
-
-//Creates a new contact with the provided values for variables:
-//      name, email, phoneNumber, city, and type
-// 
-// Groups & tags are both empty vector<string>
-Contact::Contact(string newName, string newEmail, string newNumber, string newCity, ContactType newType)
+// Setter for name of the contact
+void Contact::setName(string newName)
 {
     name = newName;
+}
+
+// Setter for email in the contact
+void Contact::setEmail(string newEmail)
+{
     email = newEmail;
+}
+
+// Setter for phone number in the contact
+void Contact::setPhoneNumber(string newNumber)
+{
     phoneNumber = newNumber;
+}
+
+// Setter for city in the contact
+void Contact::setCity(string newCity)
+{
     city = newCity;
-    type = newType; 
 }
 
-
-// =============================================================================
-// MARK: Adders
-// =============================================================================
-
-void Contact::addTag(string newTag) {
-    tags.push_back(newTag);
-}
-
-
-// =============================================================================
-// MARK: Removers
-// =============================================================================
-
-// Method takes a provided group (given by the name) and deletes it from the vector
-void Contact::removeGroup(string groupKey)
+// Setter for contact type in the contact
+void Contact::setType(ContactType newType)
 {
-    //find the item in the vector
-    //erase the item in the vector
-
-    //loop to iterate through groups assigned to a contact
-    for(int i = 0; i < groups.size(); i++)
-    {
-        if(groups[i] == groupKey)
-        {
-            groups.erase(groups.begin()+i);
-            return;
-        }
-    }
-
-    //this statement will essentially work as an else case
-    //executes only if the flow goes through the loop without finding the Key
-    cout << "The contact is not a member of group: " << groupKey << endl
-    << "Nothing was deleted." << endl << endl;
-    return;
-
+    type = newType;
 }
 
-
-// =============================================================================
-// MARK: Getters
-// =============================================================================
-
-
-
-// =============================================================================
-// MARK: Console IO
-// =============================================================================
-
-// Prints info to console line by line
-// Does not begin or end with a newline, caller is responsible for line formatting
-void Contact::printInfo() const {
-    cout << "Name: " << name << endl;
-    cout << "Email: " << email << endl;
-    cout << "Phone Number: " << phoneNumber << endl;
-    cout << "City: " << city << endl;
-    cout << "Contact Type: " << ContactTypeToString(type) << endl;
-
-    cout << "Groups: ";
-    for (int i = 0; i < groups.size(); i++) {
-        if (i < groups.size() - 1) {
-            cout << groups[i] << ", ";
-        } else {
-            cout << groups[i];
-        }
-    }
-    cout << endl;
-
-    cout << "Tags: ";
-    for (int i = 0; i < tags.size(); i++) {
-        if (i < tags.size() - 1) {
-            cout << tags[i] << ", ";
-        } else {
-            cout << tags[i];
-        }
-    }
-}
-
-
-// =============================================================================
-// MARK: File IO
-// =============================================================================
-
-// Export contact to text file.
-void Contact::exportToFile(ofstream& out)
+// Getter for name in the contact
+string Contact::getName() const
 {
-    // Print to file non-vectors
-    out << name << endl;
-    out << email << endl;
-    out << phoneNumber << endl;
-    out << city << endl;
-    out << type << endl;
-
-    // Print vectors
-    out << groups.size() << endl;
-    for (int i = 0; i < groups.size(); i++)
-    {
-        out << groups[i]  << endl;
-    }
-
-    out << tags.size()  << endl;
-    for (int i = 0; i < tags.size(); i++)
-    {
-        out << tags[i]  << endl;
-    }
+    return name;
 }
 
-// Import contact from text file.
-void Contact::importFromFile(ifstream& in)
+// Getter for email in the contact
+string Contact::getEmail() const
 {
-    short contactType{};
-    int groupSize{};
-    string group{};
-    int tagSize{};
-    string tag{};
-    
-    // Import strings
-    getline(in, name);
-    getline(in, email);
-    getline(in, phoneNumber);
-    getline(in, city);
-
-    // Import type
-    in >> contactType;
-    cin.ignore(10000, '\n');
-
-    type = ContactType(contactType);
-
-    // Import Groups
-    in >> groupSize;
-    cin.ignore(10000, '\n');
-
-    for (int _ = 0; _ < groupSize; _++)
-    {
-        getline(in, group);
-        groups.push_back(group);
-    }
-
-    // Import Tags
-    in >> tagSize;
-    cin.ignore(10000, '\n');
-
-    for (int _ = 0; _ < tagSize; _++)
-    {
-        getline(in, tag);
-        tags.push_back(tag);
-    }
+    return email;
 }
 
+// Getter for phone number in the contact
+string Contact::getPhoneNumber() const
+{
+    return phoneNumber;
+}
 
-// =============================================================================
-// MARK: UTILITIES
-// =============================================================================
+// Getter for city in the contact
+string Contact::getCity() const
+{
+    return city;
+}
 
-string ContactTypeToString(const ContactType& contactType) {
-    switch (contactType) {
-        case PERSON: return "Person";
-        case BUSINESS: return "Business";
-        case VENDOR: return "Vendor";
-        case EMERGENCY: return "Emergency";
-        default: return "";
-    }
+// Getter for contact type in the contact
+ContactType Contact::getType() const
+{
+    return type;
+}
+
+// Getter for group of the contact in the contact
+const vector<string>& Contact::getGroups() const
+{
+    return groups;
+}
+
+// Getter for the tag of the contact in the contact
+const vector<string>& Contact::getTags() const
+{
+    return tags;
 }
